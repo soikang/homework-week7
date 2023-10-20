54a3edc771e5997a942999633f7effdd8ce59982
Soi <skang0806@gmail.com>
2023-10-20 13:27:14 -0600

    added initial state for select button

--- day2/js/activity.js
+++ day2/js/activity.js
@@ -1,18 +1,33 @@
 $(document).ready(function () {
-    // Add a click event handler to all table cells in the tbody
-    $('td').click(function (){
-         // Check if the cell contains the text "Not Available"
+    // Get a reference to the table
+    var table = document.getElementById("activityTable");
+
+    // Get a reference to the "Select" button
+    var selectButton = $("#selectButton");
+
+    // Set the initial state of the button to disabled
+    selectButton.prop("disabled", true);
+
+    // Attach a click event handler to all table cells in the tbody
+    $('td').click(function () {
+        // Check if the cell contains the text "Not Available"
         if (($(this).text().trim() !== "Not Available") && !$(this).is('#unselectable')) {
             // Toggle the 'highlighted' class on the clicked cell
             $(this).toggleClass('highlighted');
 
             // Get the content of the selected cell
             var content = $(this).text().trim();
-            
+
             // Get the corresponding cliff site name from the header row
             var columnIndex = $(this).index();
             var cliffSite = $('th').eq(columnIndex).text();
-            
+
+            // Check if there are any highlighted cells
+            var anyHighlighted = $('.highlighted').length > 0;
+
+            // Enable or disable the "Select" button based on the selection state
+            selectButton.prop("disabled", !anyHighlighted);
+
             // Check if the 'displaySelected' box is visible
             if ($("#displaySelected").css("visibility") === "hidden") {
                 // If not visible, make it visible
@@ -20,16 +35,15 @@ $(document).ready(function () {
                 $("#displaySelected").css("margin-top", "2em"); // Add spaces above display box
             }
 
-            if ($(this).hasClass("highlighted")){
+            if ($(this).hasClass("highlighted")) {
                 $("#displaySelected").css("visibility", "visible"); // make display box visible
                 $("#displaySelected").css("margin-top", "2em"); // add spaces above display box
-                
+
                 // Display the activity and cliff site
                 $("#result").append("<p>" + content + " at " + cliffSite + "</p>"); // add child element with contents of cell
-            }
-            else { // if selected cell doesn't have class 
+            } else { // if selected cell doesn't have class
                 // Remove child element
-                $('#result p:contains('+content+')').remove();
+                $('#result p:contains(' + content + ')').remove();
 
                 // Check if there are any child elements within parent
                 if ($('#result').has('p').length === false) {
@@ -42,3 +56,4 @@ $(document).ready(function () {
         }
     });
 });
+
